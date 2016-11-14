8e84f64d5a31b386a3d97c34cefd208a15d36d47
rchrdchng123 <rchrdchung@gmail.com>
2016-11-14 12:03:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/a2017rchung/testproject/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.a2017rchung.testproject;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void myClickHandler(View view) {
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+}
+
